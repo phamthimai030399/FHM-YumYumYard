32f4fb92bcd8a554056b0dcc128f29140da37b51
Phạm Thị Mai <pham82755@gmail.com>
2023-12-22 17:10:33 +0700

    no message

---
+++ assets/js/contact-us.js
@@ -0,0 +1,11 @@
+const swiperContactUs = new Swiper(".contact-slider", {
+    loop: true,
+    speed: 1500,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    spaceBetween: 25,
+    slidesPerView: 2.5,
+    freeMode: true,
+    });
